b55a347fe215db558de457737cbd258d1e76fe55
robijn112 <robijn112@hotmail.com>
2017-01-17 22:47:50 +0100

    Saving progress
    added music, working on buttons

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import pygame, math, sys
-width = 1024
-higth = 768
+import pygame, math, sys, random, time
+pygame.init() # makes pygame work
+width = 800
+higth = 600
 resolution = (width,higth)
 red = (255,0,0) #configures some colors
 green = (0,255,0)
@@ -8,14 +9,49 @@ blue = (0,0,255)
 white = (255,255,255)
 black = (0,0,0)
 yellow = (255,255,0)
-pygame.init() # makes pygame work
+purple = (153,0,153)
+font = pygame.font.Font(None, 30)
+largefont = pygame.font.Font(None, 90)
 screen = pygame.display.set_mode(resolution) #sets the screen dimensions
-
 pygame.display.set_caption('Opseilen!')
-while True: # main game loop
-    for event in pygame.event.get(): 
-        if event.type == pygame.QUIT: #quits the game if you press the red button
-            pygame.quit()
-            sys.exit()
-    
+
+
+def buttons_menu():
+    play_button = pygame.draw.rect(100,100,50,25)
+
+
+
+
+def dice_roll():
+    #screen.fill(black)
+    dice_result = font.render(str(random.randint(2,12)),1,purple)
+    screen.blit(dice_result,(0.95*width,0.05*higth))
+
+
+
+def music(): #start music function   
+    pygame.mixer.music.load('background.mp3')#specifies music file
+    pygame.mixer.music.play(-1) #loops forever
+def stop_music():#stop music function
+    pygame.mixer.music.stop()
+
+def draw():
+    screen.fill(black)
+    buttons_menu
+    dice_roll
     pygame.display.flip()
+
+
+def homescreen():
+    running = True
+    while running: #homescreen loop
+        for event in pygame.event.get(): 
+            if event.type == pygame.QUIT: #quits the game if you press the red button
+                pygame.quit()
+                sys.exit()
+        draw
+
+
+
+
+homescreen()
