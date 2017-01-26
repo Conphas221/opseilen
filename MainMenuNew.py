59a8056e847702b147e20e7736e858fbf6426b70
emmadrost <0936606@hr.nl>
2017-01-26 16:58:17 +0100

    Main menu herschreven

--- MainMenuNew.py
+++ MainMenuNew.py
@@ -2,145 +2,96 @@ import pygame
 import sys
 import os
 
-from pygame.locals import *
+#from pygame.locals import *
+
+#NO GGLOBAL VARIABLES
+
+class Button:
+    def __init__(self, text, x, y, action):
+        self.X = x
+        self.Y = y
+        self.DefaultText = text
+        self.Color = (130, 130, 130)
+        self.Action = action
+
+        self.Text = (pygame.font.Font(None, 30)).render(text, 1, self.Color)
+        self.Rect = self.Text.get_rect()
+        self.Rect.topleft = (x, y)
+
+    def Update(self, events):
+        for event in events:
+            if event.type == pygame.MOUSEMOTION:
+                if self.Rect.collidepoint(event.pos):                    
+                    self.Text = (pygame.font.Font(None, 30)).render(self.DefaultText, 1, (255,255,0))            
+                else:
+                    self.Text = (pygame.font.Font(None, 30)).render(self.DefaultText, 1, (130, 130, 130))
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if self.Rect.collidepoint(event.pos):                    
+                    return self.Action()
+        return self
+        
+    def Draw(self, screen):
+        screen.blit(self.Text, self.Rect)
+        
+class InstructionMenu:
+    def __init__(self, width, height):
+        self.Width = width
+        self.Height = height        
+        self.Image = pygame.image.load(os.path.join('/Users/emmadrost/Desktop/opseilen-Working-menu-2NEW/Project2/spelregels.bmp'))
+
+        self.Buttons = [Button("Back", 10, 90, lambda : MainMenu(width, height))]
+    def Update(self):
+        events = pygame.event.get()
+        for event in events:            
+            if event.type == pygame.QUIT:
+              sys.exit()
+        for button in self.Buttons:
+            res = button.Update(events)
+            if (res != button):
+                return res
+        return self
+
+    def Draw(self, screen):
+        screen.blit(self.Image,(150, 50))
+        for button in self.Buttons:
+            button.Draw(screen)
+
+class MainMenu:
+    def __init__(self, width, height):
+        self.Width = width
+        self.Height = height        
+        self.Image = pygame.image.load(os.path.join('/Users/emmadrost/Desktop/opseilen-Working-menu-2NEW/Project2/euromast1.bmp'))
+        self.Buttons = [Button("Play", 10, 10, lambda : print("play the game")),
+                        Button("Instructions", 10, 50, lambda : InstructionMenu(width, height)),
+                        Button("Quit", 10, 90, lambda : sys.exit())]
+    def Update(self):
+        events = pygame.event.get()
+        for event in events:            
+            if event.type == pygame.QUIT:
+              sys.exit()
+        for button in self.Buttons:
+            res = button.Update(events)
+            if (res != button):
+                return res
+        return self
+
+    def Draw(self, screen):
+        screen.blit(self.Image,(150, 50))
+        for button in self.Buttons:
+            button.Draw(screen)
+
+
 
 pygame.init()
-
-screen = pygame.display.set_mode((1366, 768))
-
-# zorgt dat quit makkelijker kan worden opgeroepen
-def quitgame():
-    pygame.quit()
-    quit()
-
-class Empty:
-        def __init__(self):
-                self.IsEmpty = True
-empty = Empty()
-
-
-class Node:
-        def __init__(self, value, tail):
-                self.IsEmpty = False
-                self.Value = value
-                self.Tail = tail
-
-#Main menu screen
-class MainMenu:
-
-    hovered = False
-    
-    def __init__(self, text, pos, screen, callback):
-        
-        self.text = text
-        self.pos = pos
-        self.set_rect()
-        self.callback = callback
-        
-        self.screen = screen
-        
-        self.caption = pygame.display.set_caption('MAIN MENU')
-
-        self.font = pygame.font.Font(None, 30)
-        
-        #slaat background foto op
-        self.image = pygame.image.load(os.path.join('Project2/euromast1.bmp'))
-
-
-    def draw(self):
-        self.set_rend()
-        self.screen.blit(self.rend, self.rect)
-
-    def set_rend(self):
-        self.rend = (pygame.font.Font(None, 30)).render(self.text, 1, self.get_color())
-        
-    def get_color(self):
-        if self.hovered:
-            return (255, 255, 0)
-        else:
-            return (130, 130, 130)
-        
-    def set_rect(self):
-        self.set_rend()
-        self.rect = self.rend.get_rect()
-        self.rect.topleft = self.pos
-
-    #positie van muis op de buttons, zodat op alle delen van woorden te klikken is
-    def click(self, pos):
-        if pos[0] >= self.pos[0] and pos[0] <= self.pos[0] + 130:
-            if pos[1] >= self.pos[1] and pos[1] <= self.pos[1] + 20: 
-                self.callback()
-
-    def update(self):
-        pass
-
-# Buttons pressed
-def OnStartPressed():
-    print("open game")
-    import main
-    _game.program_loop()
-    pass
-
-def OnHighscorePressed():
-    print("open highscore")
-    pass
-
-def OnSpelregelsPressed():
-    print("open spelregels")
-    import LinkNaarRules
-    #while True:
-     #  for LinkNaarRules in MainMenu:
-      #     if 
-       # if MainMenu:
-        #    mainmenu_draw
-        #else:
-           # LinkNaarRules
-    
-    pass
-
-def OnQuitPressed():
-    print("stop game")
-    quit()
-    pass 
-
-game_options = [MainMenu("START GAME", (50, 80), screen, OnStartPressed), 
-                    MainMenu("HIGH SCORE", (50, 160), screen, OnHighscorePressed),
-                    MainMenu("SPELREGLES", (50, 240), screen, OnSpelregelsPressed), 
-                    MainMenu("QUIT GAME", (50, 320), screen, OnQuitPressed)]
-
-def mainmenu_draw():
-    for option in game_options:
-        if option.rect.collidepoint(event.pos):
-            option.hovered = True
-        else:
-            option.hovered = False
-        option.draw()
-
-
+width = 700
+height = 800
+screen = pygame.display.set_mode((width, height))
+scene = MainMenu(width, height)
 while True:
     pygame.event.pump()
     screen.fill((255,255,255))    
-    #roept background op while true
-    screen.blit(pygame.image.load(os.path.join('Project2/euromast1.bmp')),(0, 0))
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit()
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            for option in game_options:
-                    option.click(event.pos)
-        elif event.type == pygame.MOUSEMOTION:
-            mainmenu_draw()
-            pygame.display.update()
-            pygame.display.flip()
+    scene = scene.Update()
+    scene.Draw(screen)
 
-
-def program():
-    mainmenu = MainMenuNew()
-    mainmenu.program_loop()
-
-program()
-
-
-
-
+    pygame.display.flip()
